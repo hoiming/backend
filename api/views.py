cda9a4350a62af6d1119fa323fd722b2ebb279d9
Hai Ming <haiming_liang@hotmail.com>
2023-03-20 11:59:56 +0800

    using auth token: Haiming

--- api/views.py
+++ api/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from .serializers import TodoSerializer, TodoToggleCompleteSerializer
 from todo.models import Todo
 from rest_framework import generics, permissions
+from django.db import IntegrityError
+from django.contrib.auth.models import User
+from rest_framework.parsers import JSONParser
+from rest_framework.authtoken.models import Token
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 class TodoList(generics.ListAPIView):
     serializer_class = TodoSerializer
@@ -40,3 +46,18 @@ class TodoToggleComplete(generics.UpdateAPIView):
     def perform_update(self, serializer):
         serializer.instance.completed = not(serializer.instance.completed)
         serializer.save()
+
+@csrf_exempt
+def signup(request):
+    if request.method == 'POST':
+        try:
+            data = JSONParser().parse(request)
+            user = User.objects.create_user(
+                username=data['username'],
+                password=data['password']
+            )
+            user.save()
+            token = Token.objects.create(user=user)
+            return JsonResponse({'token': str(token)}, status = 201)
+        except IntegrityError:
+            return JsonResponse({'error': 'username taken, choose another username'}, status=400)
