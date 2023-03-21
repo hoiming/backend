990c7c22a48105b53f7cfa968ab98993e1edde0b
Hai Ming <haiming_liang@hotmail.com>
2023-03-21 12:45:08 +0800

    freeze depencies

--- api/urls.py
+++ api/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
 
 
     path('todos/', views.TodoList.as_view()),
-    path('todos/', views.TodoListCreate.as_view(http_method_names=['post'])),
+    path('todos', views.TodoListCreate.as_view(http_method_names=['post'])),
     path('todos/<int:pk>', views.TodoRetrieveUpdateDetroy.as_view()),
     path('todos/<int:pk>/complete', views.TodoToggleComplete.as_view(http_method_names=['put'])),
     path('signup/', views.signup),
